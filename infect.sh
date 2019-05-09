f3c2200095f2421fafd4f195f53fe517b5271e78
Ole Hansen <ole.hansen@fortis-it.de>
2019-05-09 19:49:09 +0200

    Setup dotfiles

--- infect.sh
+++ infect.sh
@@ -8,7 +8,8 @@ read -p "Username: " username
 home_dir="/home/$username"
 read -p "Set up a user? (y/n): " opt_user
 read -p "Set up yay (y/n): " opt_yay
-read -p "Install a whole bunch of stuff? (y/n): " opt_install
+read -p "Install i3 gaps and a bunch of other stuff? (y/n): " opt_install
+read -p "Set up dotfiles? (y/n): " opt_dot
 
 if [ "$opt_user" = "y" ]
 then
@@ -21,9 +22,9 @@ then
   echo "Defaults !tty_tickets" >> /etc/sudoers
   echo "Defaults passwd_timeout=60" >> /etc/sudoers
 
-  echo 'if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then' >> $home_dir/.bash_profile
-  echo '  startx' >> $home_dir/.bash_profile
-  echo 'fi' >> $home_dir/.bash_profile
+  echo 'if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then' >> "$home_dir/.bash_profile"
+  echo '  startx' >> "$home_dir/.bash_profile"
+  echo 'fi' >> "$home_dir/.bash_profile"
   chown ole:ole $home_dir/.bash_profile
 
   echo 'exec i3' >> $home_dir/.xinitrc
@@ -54,7 +55,22 @@ then
 
   pacman -Su i3 xorg-server xorg-xinit ttf-inconsolata ttf-liberation ttf-font-awesome tmux gvim keychain openssh rofi feh compton acpi sysstat alsa-utils ntfs-3g pavucontrol pulseaudio pulseaudio-bluetooth networkmanager networkmanager-openconnect network-manager-applet vlc thunar ranger gtk-chtheme arc-gtk-theme viewnior tumbler ffmpegthumbnailer gvfs gvfs-smb xarchiver redshift arandr autorandr
 
+  cd /
+  git clone https://github.com/cinocode/st.git
+  cd /st
+  make install
+  cd /
+  rm -rf /st
+
   sudo -u "$username" yay -Sy google-chrome enpass-bin system-san-francisco-font-git ttf-vlgothic screengrab pasystray pulseaudio-ctl
+fi
+
+if [ "$opt_dot" = "y" ]
+then
+  sudo -u "$username" git clone https://github.com/cinocode/dotfiles "$home_dir/.dotfiles"
+  sudo -u "$username" sh "$home_dir/.dotfiles/init_configs.sh"
+  sudo -u "$username" sh "$home_dir/.dotfiles/init_wallpaper.sh"
+  sudo -u "$username" sh "$home_dir/.dotfiles/init_vim.sh"
 fi
 
 echo If everything looks clean, you probably want to rm -rf /rice
