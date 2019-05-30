d362bfa2178bbfb6dbbe5db7a63c7eb8720db606
Ole Hansen <ole.hansen@fortis-it.de>
2019-05-30 20:07:31 +0200

    Fix yay commands and xdg user dir

--- infect.sh
+++ infect.sh
@@ -50,19 +50,19 @@ then
 
   pacman -Syyu
 
-  yay -S sway swaylock-blur-bin waybar xorg-server-xwayland 
-  yay -S ttf-inconsolata ttf-liberation ttf-dejavu otf-font-awesome system-san-francisco-font-git ttf-vlgothic
-  yay -S zsh tmux gvim keychain openssh xdg-user-dirs
-  yay -S rofi w3m feh thunar ranger
-  yay -S acpi sysstat alsa-utils ntfs-3g gotop
-  yay -S pavucontrol pulseaudio pulseaudio-bluetooth pasystray pulseaudio-ctl
-  yay -S networkmanager networkmanager-openconnect network-manager-applet-indicator
-  yay -S vlc arc-gtk-theme viewnior tumbler ffmpegthumbnailer
-  yay -S gvfs gvfs-smb xarchiver reflector
-  yay -S git maven nodejs jdk8-openjdk
-  yay -S iw dialog wpa_supplicant
-  yay -S grim slurp enpass-bin rxvt-unicode-pixbuf
-  yay -S google-chrome
+  sudo -u "$username" yay -S sway swaylock-blur-bin waybar xorg-server-xwayland 
+  sudo -u "$username" yay -S ttf-inconsolata ttf-liberation ttf-dejavu otf-font-awesome system-san-francisco-font-git ttf-vlgothic
+  sudo -u "$username" yay -S zsh tmux gvim keychain openssh xdg-user-dirs
+  sudo -u "$username" yay -S rofi w3m feh thunar ranger
+  sudo -u "$username" yay -S acpi sysstat alsa-utils ntfs-3g gotop
+  sudo -u "$username" yay -S pavucontrol pulseaudio pulseaudio-bluetooth pasystray pulseaudio-ctl
+  sudo -u "$username" yay -S networkmanager networkmanager-openconnect network-manager-applet-indicator
+  sudo -u "$username" yay -S vlc arc-gtk-theme viewnior tumbler ffmpegthumbnailer
+  sudo -u "$username" yay -S gvfs gvfs-smb xarchiver reflector
+  sudo -u "$username" yay -S git maven nodejs npm jdk8-openjdk
+  sudo -u "$username" yay -S iw dialog wpa_supplicant
+  sudo -u "$username" yay -S grim slurp enpass-bin rxvt-unicode-pixbuf
+  sudo -u "$username" yay -S google-chrome
 
   chsh -s /bin/zsh "$username"
 
@@ -70,7 +70,7 @@ then
   echo "google-chrome-stable --incognito" >> /usr/local/bin/google-chrome-stable-incognito
   chmod +x /usr/local/bin/google-chrome-stable-incognito
 
-  echo 'DESKTOP=$home_dir' > /etc/xdg/user-dirs.defaults
+  echo "DESKTOP=${home_dir}" > /etc/xdg/user-dirs.defaults
   echo 'DOWNLOAD=down' >> /etc/xdg/user-dirs.defaults
   echo 'DOCUMENTS=doc' >> /etc/xdg/user-dirs.defaults
   echo 'MUSIC=music' >> /etc/xdg/user-dirs.defaults
